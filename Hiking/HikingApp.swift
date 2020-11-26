e0242cd9432ae7d7723b30e7fe349a98f4e7a5c9
Michael Miles <michael.gage.miles@gmail.com>
2020-11-26 10:34:35 -0500

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Michael Miles on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
